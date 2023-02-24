caeaaf1ae0315497e78383addca7011cb9224b32
Arno Leruste <recrox91@hotmaiL.com>
2023-02-24 10:29:39 +0100

    avancement de la structure du projet

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Providers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +15,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+builder.Services.AddDbContext<PeopleContext>(option =>
+    option.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
